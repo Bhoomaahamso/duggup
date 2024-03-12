35e76f90eca71dd6b39cb724c9027fc15372b8dd
Bhoomaahamso <bhoomaahamso@gmail.com>
2024-03-12 16:30:54 +0530

    first version

---
+++ src/components/avatar.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<!-- <div class="flex items-center flex-col"> -->
+<div class="w-fit">
+    <img class=" min-w-[120px] max-w-[120px] rounded-full border-solid border-[#EAEFF5]-400 border-[5px]" src="person.jfif" alt="">
+    <h2 class="font-bold text-center text-[#4B5C6D] text-2xl/7 max-w-[120px] mt-2">Krishna Kiran</h2>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,36 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<script>
+    import Card from '../components/card.svelte'
+    import Avatar from '../components/avatar.svelte'
+    import Button from '../components/button.svelte'
+    import Icon from '../components/icon.svelte';
+    import Nav from '../components/Nav.svelte';
+    import ProfileInfo from '../components/ProfileInfo.svelte';
+    import Title from '../components/Title.svelte';
+    import Timeline from '../components/Timeline.svelte';
   
+  </script>
+  
+  <!-- <Card imgSrc='pc.jfif' text='No amount of technology can convert a bad story into a good story.' /> -->
+  <!-- <Avatar /> -->
+  <!-- <Button /> -->
+  <!-- <Icon /> -->
+  <!-- <Title /> -->
+  <div class="mqx-[117px] mx-[7%] max-w-[1658px] xl:mx-auto">
+  <Nav />
+  <ProfileInfo />
+  <Timeline />
+  </div>
+    
   <style lang="postcss">
-    /* :global(html) {
-      background-color: theme(colors.gray.100);
-    } */
+  
+    .con {
+        margin: 0 117px;
+    }
+  
+  
+  
+    :global(html) {
+        font-family: 'Inter';
+    }
+  
   </style>
